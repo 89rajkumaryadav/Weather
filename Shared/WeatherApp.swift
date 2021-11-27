0615ad2d2a4c27e3f3b317d126685cb277646833
Rajkumar <89rajkumaryadav@gmail.com>
2021-11-27 18:06:07 +0530

    Initial Commit

---
+++ Shared/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Shared
+//
+//  Created by Rajkumar Yadav on 27/11/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
